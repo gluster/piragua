aa683bee1a1e4a171a35d82ed09f1c7f5bccceea
Chris Holcombe <xfactor973@gmail.com>
2017-11-16 08:31:36 -0800

    Enough to test

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate clap;
 extern crate gfapi_sys;
 extern crate serde;
 #[macro_use]
+extern crate serde_derive;
+#[macro_use]
 extern crate serde_json;
 
 use std::fs::create_dir;
@@ -12,13 +14,34 @@ use std::process::Command;
 use clap::{Arg, App, SubCommand};
 use gfapi_sys::gluster::*;
 
-fn do_mount(dir: &str, gluster_host: &str, volume: &str) -> Result<(), GlusterError> {
+#[derive(Deserialize)]
+struct JsonOptions {
+    #[serde(rename = "kubernetes.io/fsType")]
+    fs_type: String,
+    #[serde(rename = "kubernetes.io/readwrite")]
+    readwrite: String,
+    #[serde(rename = "kubernetes.io/secret/key1")]
+    secret_key: String,
+}
+
+// TODO: gluster_host and volume go away and should come from json_options
+fn do_mount(
+    dir: &str,
+    gluster_host: &str,
+    volume: &str,
+    json_options: Option<&str>,
+) -> Result<(), GlusterError> {
+    println!("json_options: {:?}", json_options);
     let cluster = Gluster::connect(volume, gluster_host, 24007)?;
     let dir_path = Path::new(dir);
     // Create the mount point on the cluster
-    cluster.mkdir(&dir_path, 0644)?;
+    if !cluster.exists(&dir_path)? {
+        cluster.mkdir(&dir_path, 0644)?;
+    }
     // Create the mount point on the host
-    create_dir(&dir_path)?;
+    if !dir_path.exists() {
+        create_dir(&dir_path)?;
+    }
     let mount_cmd = Command::new("mount")
         .args(
             &[
@@ -29,12 +52,21 @@ fn do_mount(dir: &str, gluster_host: &str, volume: &str) -> Result<(), GlusterEr
             ],
         )
         .output()?;
+    if !mount_cmd.status.success() {
+        return Err(GlusterError::Error(
+            String::from_utf8_lossy(&mount_cmd.stdout).into_owned(),
+        ));
+    }
     Ok(())
 }
 
 fn do_unmount(dir: &str) -> Result<(), GlusterError> {
     let mount_cmd = Command::new("unmount").arg(dir).output()?;
-
+    if !mount_cmd.status.success() {
+        return Err(GlusterError::Error(
+            String::from_utf8_lossy(&mount_cmd.stdout).into_owned(),
+        ));
+    }
     Ok(())
 }
 
@@ -55,46 +87,57 @@ fn main() {
         .author(crate_authors!())
         .about("Gluster thin Kubernetes volumes")
         .subcommand(SubCommand::with_name("init").about("Initialize the driver"))
-        .subcommand(SubCommand::with_name("attach").about(
-            "Attach a vol to a host",
-        ))
-        .subcommand(SubCommand::with_name("detach").about(
-            "Detach a vol from a host",
-        ))
-        .subcommand(SubCommand::with_name("isattached").about(
-            "Check if a vol is attached to a host",
-        ))
+        .subcommand(
+            SubCommand::with_name("isattached")
+                .about("Check if a vol is attached to a host")
+                .arg(
+                    Arg::with_name("json_options")
+                        .help("Json options")
+                        .required(false),
+                )
+                .arg(Arg::with_name("node_name").help("Node name").required(
+                    false,
+                )),
+        )
         .subcommand(
             SubCommand::with_name("mount")
                 .about("Mount a vol at a dir")
-                .arg(Arg::with_name("mount_dir").help("mount directory").required(true)),
+                .arg(
+                    Arg::with_name("mount_dir")
+                        .help("mount directory")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("json_options")
+                        .help("Json options")
+                        .required(false),
+                ),
         )
         .subcommand(
             SubCommand::with_name("unmount")
                 .about("Unmount a vol from a dir")
-                .arg(Arg::with_name("mount_dir").help("mount directory").required(true)),
+                .arg(
+                    Arg::with_name("mount_dir")
+                        .help("mount directory")
+                        .required(true),
+                ),
         )
         .get_matches();
 
-        //Handle commands
-        if let Some(ref matches) = matches.subcommand_matches("attach") {
-            //  
-        }
-        if let Some(ref matches) = matches.subcommand_matches("detach") {
-            //  
-        }
-        if let Some(ref matches) = matches.subcommand_matches("init") {
-            println!("{}", json!({"status": "success", "capabilities": {"attach": false}}));
-        }
-        if let Some(ref matches) = matches.subcommand_matches("isattached") {
-            //  
-        }
-        if let Some(ref matches) = matches.subcommand_matches("mount") {
-            let mount_dir = matches.value_of("mount_dir").unwrap();
-            process_cmd(do_mount(mount_dir, "", ""));
-        }
-        if let Some(ref matches) = matches.subcommand_matches("unmount") {
-            let mount_dir = matches.value_of("mount_dir").unwrap();
-            process_cmd(do_unmount(mount_dir));
-        }
+    //Handle commands
+    if matches.subcommand_matches("init").is_some() {
+        println!("{}", json!({"status": "success", "capabilities": {"attach": false}}));
+    }
+    if let Some(ref matches) = matches.subcommand_matches("isattached") {
+        //
+    }
+    if let Some(ref matches) = matches.subcommand_matches("mount") {
+        let mount_dir = matches.value_of("mount_dir").unwrap();
+        let json_options = matches.value_of("json_options");
+        process_cmd(do_mount(mount_dir, "", "", json_options));
+    }
+    if let Some(ref matches) = matches.subcommand_matches("unmount") {
+        let mount_dir = matches.value_of("mount_dir").unwrap();
+        process_cmd(do_unmount(mount_dir));
+    }
 }
