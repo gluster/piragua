a7f8ad05a645896410945020dc23cf297f2f4dc7
Chris Holcombe <xfactor973@gmail.com>
2017-12-01 16:28:10 -0800

    Nearly working

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,39 @@
 #[macro_use]
 extern crate clap;
 extern crate gfapi_sys;
-//#[macro_use]
-//extern crate log;
+extern crate gluster;
+extern crate itertools;
+extern crate libc;
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate uuid;
 
 use std::collections::HashMap;
-use std::fs::create_dir;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+use std::io::Result as IOResult;
 use std::path::{Path, PathBuf};
-use std::process::Command;
 use std::sync::Mutex;
 
 use clap::{Arg, App};
 use gfapi_sys::gluster::*;
+use gluster::peer::peer_list;
+use gluster::volume::volume_info;
+use itertools::Itertools;
+use libc::{S_IRGRP, S_IWGRP, S_IXGRP, S_IRWXU, S_IRUSR, S_IWUSR, S_IXUSR};
 use rocket_contrib::Json;
-use rocket::{Request, State};
-use rocket::config::{Config, Environment};
+use rocket::{Request, Response, State};
+use rocket::http::Status;
+use rocket::http::hyper::header::Location;
 use rocket::response::status::{Accepted, Created};
+use uuid::Uuid;
 
 #[cfg(test)]
 mod tests;
-
-type MessageMap = Mutex<HashMap<String, String>>;
-
-#[derive(Debug, Deserialize)]
-struct JsonOptions {
-    #[serde(rename = "kubernetes.io/fsType")]
-    fs_type: String,
-    #[serde(rename = "kubernetes.io/readwrite")]
-    readwrite: String,
-    #[serde(rename = "kubernetes.io/secret/key1")]
-    secret_key: String,
-}
 
 #[derive(Debug, Serialize)]
 struct GlusterClusters {
@@ -70,32 +67,42 @@ struct ExpandVolumeRequest {
 struct CreateVolumeRequest {
     /// Size in GB
     size: u64,
+    clusters: Option<Vec<String>>,
     /// Name of volume. If not provided, the name of the volume
     /// will be vol_{id}
-    name: Option<String>,
+    name: String,
     durability: Option<Durability>,
+    gid: u64,
     snapshot: Snapshot,
-    clusters: Option<Vec<String>>,
 }
 
 #[derive(Deserialize, Debug, Serialize)]
 pub enum VolumeType {
     #[serde(rename = "replicate")]
-    Replicate { replica: Option<u8> }, // defaults to 2 },
+    Replicate,
     #[serde(rename = "disperse")]
-    Disperse {
-        data: Option<u8>, // defaults to 8
-        redundancy: Option<u8>, // defaults to 2
-    },
+    Disperse,
     #[serde(rename = "none")]
     None,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
+struct ReplicaDurability {
+    replica: Option<u8>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct DisperseDurability {
+    data: Option<u8>,
+    redundancy: Option<u8>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 struct Durability {
     #[serde(rename = "type")]
-    mount_type: VolumeType,
-    replicate: HashMap<String, u8>,
+    mount_type: Option<VolumeType>,
+    replicate: Option<ReplicaDurability>,
+    diperse: Option<DisperseDurability>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -120,7 +127,7 @@ struct GlusterFsMount {
 #[derive(Debug, Serialize)]
 struct Brick {
     id: String,
-    path: PathBuf, //"/gluster/brick_aaaaaad2e40df882180479024ac4c24c8/brick",
+    path: PathBuf,
     size: u64,
     node: String,
     device: String,
@@ -144,15 +151,16 @@ struct AddNodeRequest {
     cluster: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct ManagedHosts {
     manage: Vec<String>,
+    storage: Vec<String>,
 }
 
 #[derive(Debug, Deserialize)]
 struct AddDeviceRequest {
-    node: String, //": "714c510140c20e808002f2b074bc0c50",
-    name: PathBuf, //": "/dev/sdb"
+    node: String,
+    name: PathBuf,
 }
 
 #[derive(Debug, Serialize)]
@@ -170,82 +178,131 @@ struct Storage {
     used: u64,
 }
 
+#[derive(Debug, Serialize)]
+struct NodeInfoResponse {
+    zone: u8,
+    id: String,
+    cluster: String,
+    hostnames: ManagedHosts,
+    devices: Vec<DeviceInfo>,
+    state: String,
+}
+
+
+
 #[post("/clusters", format = "application/json")]
-fn create_cluster(map: State<MessageMap>) -> Created<Json<GlusterClusters>> {
+fn create_cluster(state: State<Gluster>) -> Created<Json<GlusterClusters>> {
     let clusters = GlusterClusters {
-        id: "".to_string(),
+        id: "cluster-test".to_string(),
         nodes: vec![],
         volumes: vec![],
     };
 
-    //let cluster = Gluster::connect("volume", "localhost", 24007);
-
-    let mut hashmap = map.lock().expect("map lock.");
-    //    if hashmap.contains_key(&id) {
-    //        json!({
-    //            "status": "error",
-    //            "reason": "ID exists. Try put."
-    //        })
-    //    } else {
-    //
-    //    }
     Created("".to_string(), Some(Json(clusters)))
 }
 
-#[get("/clusters/<id>", format = "application/json")]
-fn get_cluster_info(id: String, map: State<MessageMap>) -> Json<GlusterClusters> {
+#[get("/clusters/<cluster_id>", format = "application/json")]
+fn get_cluster_info(
+    cluster_id: String,
+    state: State<Gluster>,
+) -> Result<Json<GlusterClusters>, String> {
+    let mut vol_list: Vec<String> = vec![];
+
+    // Get all the peers in the cluster
+    let peers = peer_list().map_err(|e| e.to_string())?;
+    let servers: Vec<String> = peers.iter().map(|ref p| p.hostname.clone()).collect();
+
+    //List all the top level directories and return them as volumes
+    let d =
+        GlusterDirectory { dir_handle: state.opendir(&Path::new("/")).map_err(|e| e.to_string())? };
+    for dir_entry in d {
+        let dir_name = format!("{}", dir_entry.path.display());
+        // Skip the parent and current dir entries
+        if dir_name == ".." || dir_name == "." {
+            continue;
+        }
+        vol_list.push(dir_name);
+    }
+
     let clusters = GlusterClusters {
-        id: "".to_string(),
-        nodes: vec![],
-        volumes: vec![],
+        id: cluster_id,
+        nodes: servers,
+        volumes: vol_list,
     };
 
-    Json(clusters)
+    Ok(Json(clusters))
 }
 
 #[get("/clusters", format = "application/json")]
-fn list_clusters(map: State<MessageMap>) -> Json<ClusterList> {
-    let clusters = ClusterList { clusters: vec![] };
+fn list_clusters(state: State<String>) -> Json<ClusterList> {
+    // Only return the single volume as a cluster
+    let clusters = ClusterList { clusters: vec![state.inner().clone()] };
     Json(clusters)
 }
 
 #[delete("/clusters/<id>", format = "application/json")]
-fn delete_cluster(id: String, map: State<MessageMap>) {
+fn delete_cluster(id: String, state: State<Gluster>) {
     //json!({ "status": "ok" })
 }
 
-#[post("/volumes", format = "application/json", data = "<input>")]
-fn create_volume(input: Json<CreateVolumeRequest>, map: State<MessageMap>) -> Accepted<String> {
-    Accepted(Some("".into()))
-}
-
 #[get("/nodes/<id>", format = "application/json")]
-fn get_node_info(id: String) -> Json<String> {
-    Json("".into())
+fn get_node_info(id: String, state: State<Gluster>) -> Result<Json<NodeInfoResponse>, String> {
+    let hostname = {
+        let mut f = File::open("/etc/hostname").map_err(|e| e.to_string())?;
+        let mut s = String::new();
+        f.read_to_string(&mut s).map_err(|e| e.to_string())?;
+        s.trim().to_string()
+    };
+
+    let resp = NodeInfoResponse {
+        zone: 1,
+        id: id,
+        cluster: "cluster-test".into(),
+        hostnames: ManagedHosts {
+            // Everyone manages themselves
+            manage: vec![hostname.clone()],
+            storage: vec![hostname],
+        },
+        devices: vec![],
+        state: "online".into(),
+    };
+    Ok(Json(resp))
 }
 
 #[delete("/nodes/<id>", format = "application/json")]
-fn delete_node(id: String) -> Accepted<String> {
-    Accepted(Some("".into()))
+fn delete_node(id: String, state: State<Gluster>) -> Result<Response, String> {
+    //NOPE you're not allowed
+    let mut response = Response::new();
+    response.set_status(Status::new(204, "Volume created"));
+    Ok(response)
 }
 
 #[post("/nodes", format = "application/json", data = "<input>")]
-fn add_node(input: Json<AddNodeRequest>) -> Accepted<String> {
-    Accepted(Some("".into()))
+fn add_node(input: Json<AddNodeRequest>, state: State<Gluster>) -> Result<Response, String> {
+    //NOPE you're not allowed
+    let mut response = Response::new();
+    response.set_status(Status::new(204, "Volume created"));
+    Ok(response)
 }
 
 #[post("/devices", format = "application/json", data = "<input>")]
-fn add_device(input: Json<AddDeviceRequest>) -> Accepted<String> {
-    Accepted(Some("".into()))
+fn add_device(input: Json<AddDeviceRequest>, state: State<Gluster>) -> Result<Response, String> {
+    //NOPE you're not allowed
+    let mut response = Response::new();
+    response.set_status(Status::new(204, "Volume created"));
+    Ok(response)
 }
 
 #[delete("/devices/<id>", format = "application/json")]
-fn delete_device(id: String) -> Accepted<String> {
-    Accepted(Some("".into()))
+fn delete_device(id: String, state: State<Gluster>) -> Result<Response, String> {
+    //NOPE you're not allowed
+    let mut response = Response::new();
+    response.set_status(Status::new(204, "Volume created"));
+    Ok(response)
 }
 
-#[get("/devices/<id>", format = "application/json")]
-fn get_device_info(id: String) -> Json<DeviceInfo> {
+#[get("/devices/<device_id>", format = "application/json")]
+fn get_device_info(device_id: String, state: State<Gluster>) -> Json<DeviceInfo> {
     let device_info = DeviceInfo {
         name: PathBuf::from("/dev/sda"), //": "/dev/sdh",
         storage: Storage {
@@ -259,50 +316,164 @@ fn get_device_info(id: String) -> Json<DeviceInfo> {
     Json(device_info)
 }
 
-#[get("/volumes/<id>", format = "application/json")]
-fn get_volume_info(id: String, map: State<MessageMap>) -> Json<VolumeInfo> {
-    let volume_info = VolumeInfo {
-        name: "".into(),
-        id: "".into(),
-        cluster: "".into(),
-        size: 0,
+#[post("/volumes", format = "application/json", data = "<input>")]
+fn create_volume<'a>(
+    input: Json<CreateVolumeRequest>,
+    state: State<Gluster>,
+) -> Result<Response<'a>, String> {
+    println!("volume request: {:#?}", input);
+    let vol_name = if input.name == "" {
+        let u = Uuid::new_v4();
+        format!("vol_{}", u.hyphenated().to_string())
+    } else {
+        input.name.clone()
+    };
+    let dir_path = Path::new(&vol_name);
+
+    // Create the mount point on the cluster
+    if !state.exists(&dir_path).map_err(|e| e.to_string())? {
+        state.mkdir(&dir_path, S_IRWXU).map_err(|e| e.to_string())?;
+    }
+    // Change the group id on it to match the requested on
+    // root and the requesting user can read the directory
+    state.chown(&dir_path, 0, input.gid as u32).map_err(
+        |e| e.to_string(),
+    )?;
+
+    // root can read/execute and requesting user can read/write/execute
+    state
+        .chmod(&dir_path, S_IRUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP)
+        .map_err(|e| e.to_string())?;
+
+    let mut response = Response::new();
+    response.set_header(Location(format!("/volumes/{}", vol_name)));
+    response.set_status(Status::new(303, "Volume created"));
+
+    Ok(response)
+}
+
+fn gluster_vols(vol_id: &str) -> IOResult<HashMap<String, String>> {
+    let vol_file = File::open(format!("/var/lib/glusterd/vols/{}/info", vol_id))?;
+    let mut vol_data = HashMap::new();
+    let f = BufReader::new(vol_file);
+    for line in f.lines() {
+        let l = line?;
+        let parts: Vec<&str> = l.split("=").collect();
+        if parts.len() != 2 {
+            // Skip broken data
+            continue;
+        }
+        vol_data.insert(parts[0].to_string(), parts[1].to_string());
+    }
+    Ok(vol_data)
+}
+
+#[get("/volumes/<vol_id>", format = "application/json")]
+fn get_volume_info(
+    vol_id: String,
+    state: State<Gluster>,
+    vol_name: State<String>,
+) -> Result<Json<VolumeInfo>, String> {
+    // Use this to get the backup-volfile-server info
+    //let vol_info = volume_info(&vol_name).map_err(|e| e.to_string())?;
+    let vol_info = gluster_vols(&vol_name).map_err(|e| e.to_string())?;
+    let peers = peer_list().map_err(|e| e.to_string())?;
+
+    let mut brick_info: Vec<Brick> = Vec::new();
+    for item in &vol_info {
+        if item.0.starts_with("brick") {
+            /*
+            brick_info.push(Brick {
+                id: "".into(),
+                //TODO: what is this supposed to be?
+                path: brick.path, //"/gluster/brick_aaaaaad2e40df882180479024ac4c24c8/brick",
+                size: 10,
+                //TODO: what do I return here for node and device?
+                node: "".into(),
+                device: "".into(),
+            });
+            */
+        }
+    }
+    let backup_servers: Vec<String> = peers.iter().map(|ref p| p.hostname.clone()).collect();
+
+    let mut mount_options: HashMap<String, String> = HashMap::new();
+    mount_options.insert(
+        "backup-volfile-servers".into(),
+        backup_servers.iter().join(",").to_string(),
+    );
+
+    let response = VolumeInfo {
+        name: vol_id.clone(),
+        id: vol_info.get("volume-id").unwrap().clone(), //.hyphenated().to_string(),
+        cluster: "cluster-test".into(),
+        size: 10,
         durability: Durability {
-            mount_type: VolumeType::Replicate { replica: Some(3) },
-            replicate: HashMap::new(),
+            mount_type: Some(VolumeType::Replicate),
+            replicate: Some(ReplicaDurability { replica: Some(3) }),
+            diperse: None,
         },
         snapshot: Snapshot {
             enable: Some(true),
-            factor: Some(0.00),
+            factor: Some(1.20),
         },
         mount: Mount {
             glusterfs: GlusterFsMount {
-                device: "".into(),
-                options: HashMap::new(),
+                device: format!(
+                    "{server}:/{volume}/{path}",
+                    server = peers[0].hostname,
+                    volume = *vol_name,
+                    path = vol_id
+                ),
+                options: mount_options,
             },
         },
         bricks: vec![],
     };
-    Json(volume_info)
+    println!("VolumeInfo: {:#?}", response);
+    Ok(Json(response))
 }
 
 #[post("/volumes/<id>/expand", format = "application/json", data = "<input>")]
-fn expand_volume(
-    id: String,
-    input: Json<ExpandVolumeRequest>,
-    map: State<MessageMap>,
-) -> Accepted<String> {
-    Accepted(Some("".into()))
+fn expand_volume(id: String, input: Json<ExpandVolumeRequest>, state: State<Gluster>) -> Response {
+    let mut response = Response::new();
+    response.set_status(Status::new(204, "Volume expanded"));
+    response
 }
 
-#[delete("/volumes/<id>", format = "application/json")]
-fn delete_volume(id: String, map: State<MessageMap>) -> Accepted<String> {
-    Accepted(Some("".into()))
+#[delete("/volumes/<vol_id>")]
+fn delete_volume<'a>(vol_id: String, state: State<Gluster>) -> Result<Response<'a>, String> {
+    // Clients will keep calling this and we need to return 204 when it's finished
+    // This works out well because rm -rf could take awhile.
+    let mut response = Response::new();
+    response.set_status(Status::Accepted);
+    response.set_header(Location(format!("/volumes/{}", vol_id)));
+
+    // Delete the directory.
+    // TODO: How can we background this and tell the client to come back later?
+    state.remove_dir_all(&Path::new(&vol_id)).map_err(
+        |e| e.to_string(),
+    )?;
+
+    Ok(response)
 }
 
 #[get("/volumes", format = "application/json")]
-fn list_volumes(map: State<MessageMap>) -> Json<VolumeList> {
-    let volumes = VolumeList { volumes: vec![] };
-    Json(volumes)
+fn list_volumes(state: State<Gluster>) -> Result<Json<VolumeList>, String> {
+    let mut vol_list: Vec<String> = vec![];
+    let d =
+        GlusterDirectory { dir_handle: state.opendir(&Path::new("/")).map_err(|e| e.to_string())? };
+    for dir_entry in d {
+        let dir_name = format!("{}", dir_entry.path.display());
+        // Skip the parent and current dir entries
+        if dir_name == ".." || dir_name == "." {
+            continue;
+        }
+        vol_list.push(dir_name);
+    }
+    let volumes = VolumeList { volumes: vol_list };
+
+    Ok(Json(volumes))
 }
 
 #[error(500)]
@@ -313,69 +484,6 @@ fn internal_error() -> &'static str {
 #[error(400)]
 fn not_found(req: &Request) -> String {
     format!("I couldn't find '{}'. Try something else?", req.uri())
-}
-
-// TODO: gluster_host and volume go away and should come from json_options
-/*
-fn do_mount(
-    dir: &str,
-    gluster_host: &str,
-    volume: &str,
-    json_options: Option<&str>,
-) -> Result<(), GlusterError> {
-    println!("json_options: {:?}", json_options);
-    let cluster = Gluster::connect(volume, gluster_host, 24007)?;
-    let dir_path = Path::new(dir);
-    // Create the mount point on the cluster
-    if !cluster.exists(&dir_path)? {
-        cluster.mkdir(&dir_path, 0644)?;
-    }
-    // Create the mount point on the host
-    if !dir_path.exists() {
-        create_dir(&dir_path)?;
-    }
-    let mount_cmd = Command::new("mount")
-        .args(
-            &[
-                "-t",
-                "glusterfs",
-                &format!("{}:/{}/{}", gluster_host, volume, dir),
-                dir,
-            ],
-        )
-        .output()?;
-    if !mount_cmd.status.success() {
-        return Err(GlusterError::Error(
-            String::from_utf8_lossy(&mount_cmd.stdout).into_owned(),
-        ));
-    }
-    Ok(())
-}
-
-fn do_unmount(dir: &str) -> Result<(), GlusterError> {
-    let mount_cmd = Command::new("unmount").arg(dir).output()?;
-    if !mount_cmd.status.success() {
-        return Err(GlusterError::Error(
-            String::from_utf8_lossy(&mount_cmd.stdout).into_owned(),
-        ));
-    }
-    Ok(())
-}
-
-fn process_cmd(res: Result<(), GlusterError>) {
-    match res {
-        Ok(_) => {
-            println!("{}", json!({"status": "Success"}));
-        }
-        Err(e) => {
-            println!("{}", json!({"status": "Failure", "message": e.to_string()}));
-        }
-    }
-}
-*/
-
-fn gluster_conn(vol: &str) -> Gluster {
-    Gluster::connect(vol, "localhost", 24007).unwrap()
 }
 
 fn rocket() -> rocket::Rocket {
@@ -417,9 +525,20 @@ fn main() {
                 .takes_value(true),
         )
         .get_matches();
+    // This is safe.  clap enforces that this is required
+    let volname = matches.value_of("volume").unwrap();
 
+    println!("Connecting to: gluster vol {}", volname);
+    let gluster = match Gluster::connect(volname, "localhost", 24007) {
+        Ok(conn) => conn,
+        Err(e) => {
+            println!("Failed to connect to gluster: {}.  Exiting", e.to_string());
+            return;
+        }
+    };
 
     rocket()
-        //.manage(gluster_conn(matches.value_of("volume").unwrap()))
+        .manage(gluster)
+        .manage(volname.to_string())
         .launch();
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Read;
+
 use rocket;
 use rocket::local::Client;
 use rocket::http::{Status, ContentType};
@@ -14,33 +17,44 @@ fn post_get_put_get() {
     //    assert_eq!(res.status(), Status::NotFound);
 
     // Create a new cluster
+    let mut f = File::open("tests/create_volume").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+
     let res = client
-        .post("/clusters")
+        .post("/volumes")
         .header(ContentType::JSON)
-        //.body(r#"{ "contents": "Hello, world!" }"#)
+        .body(s)
         .dispatch();
 
-    assert_eq!(res.status(), Status::Created);
+    println!("response: {:?}", res);
 
-    /*
+    assert_eq!(res.status(), Status::Accepted);
+
     // Check that the message exists with the correct contents.
     let mut res = client
-        .get("/message/1")
+        .get("/volumes/test")
         .header(ContentType::JSON)
         .dispatch();
     assert_eq!(res.status(), Status::Ok);
-    let body = res.body().unwrap().into_string().unwrap();
-    assert!(body.contains("Hello, world!"));
+    println!(
+        "/volumes/test response: {:?}",
+        res.body().unwrap().into_string()
+    );
 
     // Change the message contents.
-    let res = client
-        .put("/message/1")
+    let mut res = client
+        .get("/clusters/cluster-test")
         .header(ContentType::JSON)
-        .body(r#"{ "contents": "Bye bye, world!" }"#)
         .dispatch();
 
     assert_eq!(res.status(), Status::Ok);
+    println!(
+        "/clusters/cluster-test response: {:?}",
+        res.body().unwrap().into_string()
+    );
 
+    /*
     // Check that the message exists with the updated contents.
     let mut res = client
         .get("/message/1")
